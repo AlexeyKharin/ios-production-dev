7b9f646ac44f93453247003cedf6fd485f76ecfa
AlexeyKharin <9goofy6@gmail.com>
2021-07-20 22:20:53 +0300

    add implementation

--- Task2/Navigation/FeedViewController.swift
+++ Task2/Navigation/FeedViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import SnapKit
+
 
 final class FeedViewController: UIViewController {
     
@@ -25,6 +27,14 @@ final class FeedViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print(type(of: self), #function)
+        view.addSubview(stack)
+        stack.snp.makeConstraints { (make) -> Void in
+            make.left.equalTo(view.safeAreaLayoutGuide).offset(16)
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(200)
+            make.right.equalTo(view.safeAreaLayoutGuide).offset(-16)
+        }
+        stack.addArrangedSubview(buyButton)
+        stack.addArrangedSubview(buyButtonTwo)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -55,6 +65,47 @@ final class FeedViewController: UIViewController {
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         print(type(of: self), #function)
+    }
+    
+    var stack: UIStackView = {
+        let stack = UIStackView()
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.spacing = 10
+        stack.layer.borderWidth = 0.5
+        stack.layer.borderColor = UIColor.lightGray.cgColor
+        stack.axis = .vertical
+        stack.clipsToBounds = true
+        return stack
+    }()
+    lazy var buyButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Post", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .systemGray
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 16)
+        button.layer.masksToBounds = true
+        button.addTarget(self, action: #selector(press), for: .touchUpInside)
+        return button
+    }()
+    lazy var buyButtonTwo: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Post", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .blue
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 16)
+        button.layer.masksToBounds = true
+        button.addTarget(self, action: #selector(press), for: .touchUpInside)
+        return button
+    }()
+    
+    @objc func press() {
+        if let vc = PostViewController.storyboardInstance() {
+            let ui = UINavigationController(rootViewController: vc)
+            navigationController?.present(ui, animated: true, completion: nil)
+            
+    }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
--- Task2/Navigation/PostViewController.swift
+++ Task2/Navigation/PostViewController.swift
@@ -1,17 +1,23 @@
-//
-//  PostViewController.swift
-//  Navigation
-//
-//  Created by Artem Novichkov on 12.09.2020.
-//  Copyright © 2020 Artem Novichkov. All rights reserved.
-//
+
+
+
+
+
+
 
 import UIKit
 
 class PostViewController: UIViewController {
     
+    static func storyboardInstance() -> PostViewController? {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        return storyboard.instantiateViewController(withIdentifier: String(describing: PostViewController.self)) as? PostViewController
+    }
+    
+    
     var post: Post?
-
+  
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = post?.title
